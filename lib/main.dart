aa747cd34638fcd0dfcbf265ed5e5710e4e6fc4a
Abraham Diress <a.diress@alustudent.com>
2023-02-21 21:09:52 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:mini_app_gridview/HomeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.white),
+      home:Home_Screen()
+    );
+  
+  }
+}
